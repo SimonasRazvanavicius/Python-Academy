849554c9b2d4ef22d814df2275c3a766f4f8f2f2
Simonas Razvanavicius <simonas.razvanavicius@bazaarvoice.com>
2018-11-02 23:47:59 +0200

    adding module 1 code

---
+++ module 1.py
@@ -0,0 +1,36 @@
+# --------- MODULE 1: --------- #
+
+
+# -- MOD01_1-1.1_Intro_Python --#
+
+# [Task 1]
+# Program: "Hello World!" with comment
+print("Hellow World!")
+
+# [Task 2]
+# Insert a new Code cell below with a comment describing the task
+# edit cell: add print() with the message "after edit, save!"
+# run the cell
+print("after edit, save!")
+
+# Insert another new cell
+# Insert a new Code cell below
+# edit cell: add print() with the message showing the keyboard Shortcut to save Ctrl + s
+# run the cell
+print("Ctrl + s")
+
+# ----- End of sub-module ------#
+
+
+
+
+# -- MOD01_1-1.2_Intro_Python --#
+
+# [Task 1]
+# print messages with "double quotes" and 'single' quotes¶
+# [ ] enter a string in the print() function using single quotes
+print('Yellow World 1!')
+# [ ] enter a string in the print() function using double quotes
+print("Yellow World 2!")
+
+# ----- End of sub-module ------#

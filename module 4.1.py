5451ca1932294ff1bfcb5a501a20e2497f1655a7
Simonas Razvanavicius <simonas.razvanavicius@bazaarvoice.com>
2019-03-22 23:19:54 -0500

    updated with module 4.1.py

--- module 4.1.py
+++ module 4.1.py
@@ -104,4 +104,29 @@ print("Hello\nWorld!")
 
 # ----- End of sub-module ------#
 
+# --Practice_MOD04_1-6_IntroPy--#
+
+# [Task 1]
+# [ ] print a string that outputs the following exactly: The new line character is "\n"
+print("The new line character is \"\\n\"")
+
+# [Task 2]
+# [ ] print output that is exactly (with quotes): "That's how we escape!"
+print("\"That\'s how we escape!\"")
+
+
+# [Task 3]
+# [ ] with only 1 print statement and using No Space Characters, output the text commented below  
+
+# 1       one
+# 22      two
+# 333     three
+
+print("1\tone\n22\ttwo\n333\tthree")
+
+# [Task 4]
+
+
+# ----- End of sub-module ------#
+
 # ------- End of module --------#
